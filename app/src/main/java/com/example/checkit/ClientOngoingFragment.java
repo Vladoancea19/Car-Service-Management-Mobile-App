396ff01c2c81b9fcad19647501832eb927803390
Vlad <oancea.vlad19@yahoo.com>
2023-02-20 13:43:43 +0200

    Client Info - gata Bug la scanare cod QR

---
+++ app/src/main/java/com/example/checkit/ClientOngoingFragment.java
@@ -0,0 +1,19 @@
+package com.example.checkit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ClientOngoingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_client_ongoing, container, false);
+    }
+}
